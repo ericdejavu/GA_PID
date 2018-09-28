6d37e6d4755d4af9f233a48bbbb00cbf9278537d
吴嘉鑫 <m17805802047@163.com>
2018-09-28 11:24:42 +0800

    [master] add tcp control to ga

--- libs/genetic_algorithm.py
+++ libs/genetic_algorithm.py
@@ -3,13 +3,16 @@ import random
 
 class GeneticAlgorithm:
     def __init__(self):
-        self.population_pool = []
-        self.max_population  = MAX_POPULATION
-        self.dna = {}
-        self.score      = INIT
-        self.mutantion  = INIT
-        self.tense_score = TENSE_SCORE
-        self.generation_count = INIT
+        self.population_pool    = []
+        self.max_population     = MAX_POPULATION
+        self.dna                = {}
+        self.score              = INIT
+        self.mutantion          = INIT
+        self.tense_score        = TENSE_SCORE
+        self.generation_count   = INIT
+        self.control_mode       = TCP_CONGESTION_CONTROL
+        self.grow_mode          = LINEAR_INC
+        self.tickets            = {'waterfall_cnt':INIT}
 
     def mutant(self):
         self.mutantion = random.uniform(-2,2)
@@ -23,10 +26,33 @@ class GeneticAlgorithm:
             population_pool.append(self.dna)
             for key in self.dna.keys():
                 self.dna[key] = random.choice(parent)[key]
-
         self.generation_count += 1
         if random.random() < 0.2:
             self.mutant()
+
+    # act as tcp control
+    def waterfall(self,k):
+        if self.control_mode == TCP_CONGESTION_CONTROL:
+            if self.tickets['waterfall_cnt'] > WATERFALL_THRESHOLD:
+                self.grow_mode = EXPONENTIAL_GROWTH
+
+            if self.grow_mode == LINEAR_INC:
+                self.tense_score += k*TENSE_SCORE
+            elif self.grow_mode = EXPONENTIAL_GROWTH:
+                self.tense_score += TENSE_SCORE**(1/k)
+
+            if self.tense_score > MAX_SCORE:
+                self.tense_score = MAX_SCORE
+
+            # reaper kill the all
+            if len(self.population_pool) == KILL_ALL:
+                self.tense_score = TENSE_SCORE
+                self.control_mode = LINEAR_INC
+                self.tickets['waterfall_cnt'] = INIT
+
+            self.tickets['waterfall_cnt'] += WATERFALL_INC
+
+
 
     def reaper(self):
         sorted(self.population_pool,lambda x,y:cmp(x['score'],y['score']))
@@ -34,4 +60,4 @@ class GeneticAlgorithm:
             if dna['score'] < self.tense_score:
                 self.population_pool = self.population_pool[:i]
                 break
-        self.tense_score += TENSE_SCORE
+        self.waterfall(CONST_K)
