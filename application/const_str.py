62b5beb0b965844482dbec62fdbf46331e27d0c1
eric <m17805802047@163.com>
2018-10-22 23:49:59 +0800

    [ga_pid_test] change structure

---
+++ application/const_str.py
@@ -0,0 +1,79 @@
+
+# ga
+INIT = 0
+KILL_ALL = 1
+WATERFALL_INC = 1
+
+TEST_SETTING = 10000
+
+WATERFALL_THRESHOLD = TEST_SETTING
+
+TCP_CONGESTION_CONTROL = 0
+MUTANT_RATE = 0.5
+
+ORIGIN = 'org'
+BEHAVE = 'behave'
+STATIC_SCORE = 'static_score'
+EXECUTABLE_SCORE = 'executable_score'
+DYNAMIC_STABLE_SCORE = 'dynamic_stable_score'
+
+LINEAR_INC = 0
+EXPONENTIAL_GROWTH = 1
+
+## adjust
+MIN_POPULATION = 2
+MAX_POPULATION = TEST_SETTING
+KP_INIT = 2
+KI_INIT = 0
+KD_INIT = 2
+TENSE_SCORE = 0.0001
+NORMAL_TENSE_SCORE = 0.01
+TENSE_SCORE_K = 100
+LAST_GEN_COUNT = 10
+
+CONST_K = 0.5
+
+MAX_KI = 0.001
+MAX_SCORE = 0.5
+MAX_OUTPUT = 100
+
+# data record
+DB_ID = 'id'
+CREATE_TABLE_HEAD = 'CREATE TABLE IF NOT EXISTS PID_RECORD_HISTORY '
+CREATE_TABLE_PK = '( id int UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,'
+CREATE_TABLE_FH = CREATE_TABLE_HEAD + CREATE_TABLE_PK
+CREATE_TABLE_END = 'updated timestamp,created timestamp) charset=utf8;'
+COLUMN_TYPE = ' varchar(15)'
+
+TABLE_STRUCTURE = 'DESC PID_RECORD_HISTORY'
+ALTER_TABLE_HEAD = 'ALTER TABLE ADD '
+
+INSERT_HEAD = 'INSERT INTO PID_RECORD_HISTORY '
+INSERT_MID = ') VALUES '
+INSERT_END = ');'
+
+GET_PROJECT_DATA = 'select * from pid_origin where project_group={} and pid_group={}'
+
+PROJECT_TABLE_NAME = 'project_pid'
+ORIGIN_TABLE_NAME = 'origin_pid'
+MEASURE_TABLE_NAME = 'measure_pid'
+ANALYSE_TABLE_NAME = 'analyze_pid'
+
+INSERT_PROJECT = 'insert into '+PROJECT_TABLE_NAME+' (name,component_group_name,component_name) values ({},{},{});'
+INSERT_ORIGIN = 'insert into '+ORIGIN_TABLE_NAME+' (project_group,pid_group,p,i,d,max_pid_out) values ({},{},{},{}, {},{},{},{});'
+INSERT_MEASURE = 'insert into '+MEASURE_TABLE_NAME+' (group_sequence,continue_group_sequence,sub_group_sequence,turn_angle,light_trigger) values ({},{},{},{}, {},{});'
+INSERT_ANALYSE = 'insert into '+ANALYSE_TABLE_NAME+' (group_sequence,sub_group_sequence,first_react_delta_time,first_response_delta_time,peak,stable) values ({},{},{},{}, {},{},{},{});'
+
+UPDATE_ORIGIN = 'update '++'where id={}'
+
+DEFALUT_GEN_PROJECT_GROUP = 0
+
+# BenchMark
+PASSBY_UP = 1
+PASSBY_DOWN = 2
+REACT = 'react'
+RESPONESE = 'response'
+
+## adjust
+SET_DATA = 0
+INIT_DATA = 0
---
+++ application/data_record.py
@@ -0,0 +1,84 @@
+# -*- coding:utf-8 -*-
+import json,MySQLdb
+
+# data is important so no deletetion and update
+class DataRecord:
+    def __init__(self,dna):
+        self.columns = dna.keys()
+        with open('private/mysql.json') as f:
+            json.dump(sqli,f)
+        self.db = MySQLdb.connect(sqli['host'],sqli['user'],sqli['passwd'],sqli['db'],charset=sqli['charset'])
+        self.cursor = db.cursor()
+        self.update_project_group()
+        self.update_pid_group()
+
+    def dynamic_create_table(self):
+        create_table_body = ', '.join([column + COLUMN_TYPE for column in columns])
+        create_table_sql = CREATE_TABLE_FH + create_table_body + CREATE_TABLE_END
+        self.cursor.execute(create_table_sql)
+        self.db.commit()
+        self.cursor.execute(TABLE_STRUCTURE)
+        columns = self.cursor.fetch()
+        # columns dynamic
+        add_columns = set(self.columns) - set(columns)
+        for column in add_columns:
+            alter_table = ALTER_TABLE_HEAD + '(' + column + COLUMN_TYPE + ');'
+            self.cursor.execute(alter_table)
+            self.db.commit()
+
+
+    def __del__(self):
+        self.db.close()
+
+    def update_project_group(self):
+        with open() as f:
+            json.dump()
+        self.project_group =
+
+    def update_pid_group(self):
+        with open() as f:
+            json.dump()
+        self.pid_group =
+
+    def update_sub_group_sequence(self):
+        with open() as f:
+            json.dump()
+        self.sub_group_sequence =
+
+    def update_continue_group_sequence(self):
+        with open() as f:
+            json.dump()
+        self.continue_group_sequence =
+
+    def insert_id(self):
+        self.db.insert_id()
+
+    def execute(self,insert_sql):
+        self.cursor.execute(insert_sql)
+        self.db.commit()
+
+    def save_origin(self,org):
+        insert_sql = INSERT_ORIGIN.format(
+            self.project_group,self.pid_group,
+            org['kp'],org['ki'],org['kd'],MAX_OUTPUT,
+        )
+        self.execute(insert_sql)
+        return self.insert_id()
+
+    def save_measure(self,angles):
+        insert_sql = INSERT_MEASURE.format(
+            self.pid_group,self.continue_group_sequence,self.sub_group_sequence
+            angles['angle'],angles['light']
+        )
+        self.execute(insert_sql)
+
+    def save_analyse(self,analysis):
+        insert_sql = INSERT_ANALYSE.format(
+            self.pid_group,self.sub_group_sequence,
+            analysis['first_react_delta_time'],
+            analysis['first_response_delta_time'],
+            analysis['peak'],
+            analysis['stable']
+        )
+        self.execute(insert_sql)
+        return self.insert_id()
