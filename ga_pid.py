1177626d17f80518335a0899b4493b8672ca8f23
吴嘉鑫 <m17805802047@163.com>
2018-10-24 16:31:17 +0800

    [master] basic structure

--- ga_pid.py
+++ ga_pid.py
@@ -22,3 +22,75 @@ class GAPID:
         pid = self.GA.run()
         self.PID.tune(pid)
         return self.GA.dna['score']
+
+    def init_test(self,pid):
+        self.PID.clear()
+        self.PID.set_limit(max_err=180.0, max_out=100.0)
+        self.PID.tune(pid)
+        self.angle = 0
+        self.preangle = 0
+        self.v = 0
+        self.angle_set = 90
+        self.init_bench()
+
+    def init_bench(self):
+        self.benchmark.clear(self.angle_set,self.angle)
+
+    def bench_run(self,data):
+        self.benchmark.online_update(data)
+
+    def bench_score(self):
+        self.GA.dna[BEHAVE][STATIC_SCORE] = self.benchmark.get_static_score()
+        self.GA.dna[BEHAVE][EXECUTABLE_SCORE] = self.benchmark.get_executable_score()
+        if self.GA.dna[BEHAVE][STATIC_SCORE] > 0.0001 and self.GA.dna[ORIGIN]['kd'] < 0:
+            self.benchmark.print_bench()
+
+    def pid_test(self, dt, k):
+        err = self.angle_set - self.angle
+        out = self.PID.run(err)
+        self.angle += sim(self.v, out, dt, set_val=self.angle_set)
+
+        self.bench_run(self.angle)
+        self.v = self.angle - self.preangle
+        self.preangle = self.angle
+
+        if self.angle < 0:
+            self.angle = 0
+        elif self.angle > 180:
+            self.angle = 180
+        return self.angle, out
+
+    def run_test(self):
+        print 'run benchmark ...'
+        datas = []
+        for j in range(200):
+            lx,ly = [],[]
+            # print j,' --------------'
+            # print self.GA.dna[ORIGIN]
+            self.init_test(self.GA.dna[ORIGIN])
+            for i in range(500):
+                cache_out = self.pid_test(0.01, k)
+                # print i,cache_out
+                lx.append(i)
+                ly.append(cache_out[0])
+            self.bench_score()
+            datas.append([lx,ly,str(self.GA.dna)])
+            self.GA.run()
+
+        datas = []
+
+        for dna in self.GA.population_pool:
+            lx,ly = [],[]
+            self.init_test(dna[ORIGIN])
+            for i in range(500):
+                cache_out = self.pid_test(0.01, k)
+                # print i,cache_out
+                lx.append(i)
+                ly.append(cache_out[0])
+            datas.append([lx,ly,""])
+        print '-- alive population_pool size --:',len(datas)
+
+        self.graph.draw(datas)
+
+ga_pid = GAPID()
+ga_pid.run_test()
